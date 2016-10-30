1193d0902eea1305a54260136e81cd914a2737fc
Jarrod Nix <nix.jarrod@gmail.com>
2016-10-29 21:06:21 -0400

    Fix docs_url path

--- system/user/addons/rd_critical_css/addon.setup.php
+++ system/user/addons/rd_critical_css/addon.setup.php
@@ -3,7 +3,7 @@
 return array(
 	'author'      		=> 'Reusser Design',
 	'author_url'  		=> 'http://reusserdesign.com',
-	'docs_url'			=> 'https://github.com/reusserdesign/RDCriticalCSS',
+	'docs_url'			=> 'https://github.com/reusserdesign/RD-Critical-CSS',
 	'name'        		=> 'RD Critical CSS',
 	'description' 		=> 'Inlines the contents of the specified css file into the head to prevent render-blocking. Adds compressed loadCSS and cssrelpreload scripts v1.2.0 (https://github.com/filamentgroup/loadCSS) as a rel=preload polyfill.',
 	'version'     		=> '3.0.4',
